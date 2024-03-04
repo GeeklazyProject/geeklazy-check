c9a5725d99314511412b57d82743dedb2f1ebec1
weiyang.li <weiyang.li@shuyun.com>
2024-03-04 17:13:30 +0800

    feat: docker&spectrum init --task=CHECK-1 --user=geeklazy

--- geeklazy-check-service/docker-build.sh
+++ geeklazy-check-service/docker-build.sh
@@ -1,3 +1,8 @@
+if [ $# -lt 3 ]; then
+  echo fail
+  exit 1
+fi
+
 GIT_SOURCE=${1}
 GIT_BRANCH=${2}
 DOCKER_IMAGE=${3}
@@ -5,18 +10,29 @@ DOCKER_IMAGE=${3}
 baseDir="$(pwd)"
 buildDir="check-build-tmp"
 
+echo "mkdir -p ${baseDir}/${buildDir}"
 mkdir -p "${baseDir}/${buildDir}"
 
+echo "cd ${baseDir}/${buildDir}"
 cd "${baseDir}/${buildDir}"
 
-git clone -b ${GIT_SOURCE} ${GIT_BRANCH}
+echo "git clone -b ${GIT_BRANCH} ${GIT_SOURCE}"
+git clone -b ${GIT_BRANCH} ${GIT_SOURCE}
 
+echo "cd geeklazy-check/"
 cd "geeklazy-check/"
 
+echo "mvn -Dmaven.test.skip=true clean package"
 mvn "-Dmaven.test.skip=true" clean package
 if [ "$?" != "0" ]; then
   echo fail
   exit 1
 fi
 
-docker build -t "${DOCKER_IMAGE}" ${baseDir}/${buildDir}/geeklazy-check/
+echo "docker build -t "${DOCKER_IMAGE}" ${baseDir}/${buildDir}/geeklazy-check/geeklazy-check-service/"
+docker build -t "${DOCKER_IMAGE}" ${baseDir}/${buildDir}/geeklazy-check/geeklazy-check-service/
+
+cd ${baseDir}
+cp ${baseDir}/${buildDir}/geeklazy-check/geeklazy-check-service/docker-run.sh ${baseDir}/check-docker-run.sh
+chmod +x check-docker-run.sh
+rm -rf ${baseDir}/${buildDir}
--- geeklazy-check-service/docker-run.sh
+++ geeklazy-check-service/docker-run.sh
@@ -3,4 +3,5 @@ SPECTRUM_KEY=${2}
 SPECTRUM_SECRET=${3}
 DOCKER_IMAGE=${4}
 
+docker rm -f geeklazy-spectrum
 docker run -d --name geeklazy-check --network geeklazy-net -e SPECTRUM_HOST=${SPECTRUM_HOST} -e SPECTRUM_KEY=${SPECTRUM_KEY} -e SPECTRUM_SECRET=${SPECTRUM_SECRET} geeklazy-check:1.0-SNAPSHOT
