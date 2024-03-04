6b0403bdd4cdf709c990a5c432277973b41d6350
weiyang.li <weiyang.li@shuyun.com>
2024-03-04 14:47:05 +0800

    feat: timezone --task=CHECK-1 --user=geeklazy

--- geeklazy-check-service/src/main/java/com/geeklazy/check/record/controller/CheckRecordController.java
+++ geeklazy-check-service/src/main/java/com/geeklazy/check/record/controller/CheckRecordController.java
@@ -25,7 +25,7 @@ import java.util.Optional;
  */
 @Slf4j
 @RestController
-@RequestMapping("/check/record")
+@RequestMapping("/web/check/record")
 public class CheckRecordController implements CheckRecordApi {
 
     @Autowired
